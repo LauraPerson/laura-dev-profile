95dff57fa2390107ef686f47445e81b522cd99bb
laura person <lauraperson29800@gmail.com>
2020-10-21 23:04:41 +0200

    add particles JS, works

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
-
+require("particles.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -14,3 +14,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import { particleJS } from '../components/particles';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  particleJS();
+});
